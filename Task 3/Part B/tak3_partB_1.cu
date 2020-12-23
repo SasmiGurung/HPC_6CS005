5e8a3fcb04b1d8a09bdcc6cdbc91cb11edcff83b
Sasmita Gurung <S.Gurung12@wlv.ac.uk>
2020-12-24 00:58:35 +0545

    feat: Add codes for task 3

---
+++ Task 3/Part B/tak3_partB_1.cu
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <malloc.h>
+#include <signal.h>
+#include <cuda_runtime_api.h>
+#include "lodepng.h"
+
+int time_difference(struct timespec *start, struct timespec *finish,
+                    long long int *difference) {
+  long long int ds =  finish->tv_sec - start->tv_sec;
+  long long int dn =  finish->tv_nsec - start->tv_nsec;
+
+  if(dn < 0 ) {
+    ds--;
+    dn += 1000000000;
+  }
+  *difference = ds * 1000000000 + dn;
+  return !(*difference > 0);
+}
