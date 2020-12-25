43cb1d269acb5d58c5c421689c864d0ed7381e42
Sasmita Gurung <S.Gurung12@wlv.ac.uk>
2020-12-25 17:08:36 +0545

    feat: Add password cracking with cuda

--- Task 3/Part A/task3_partA_1.cu
+++ Task 3/Part A/task3_partA_1.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cuda_runtime_api.h>
 #include <time.h>
 
@@ -24,130 +25,124 @@
   University Email: S.Gurung12@wlv.ac.uk
 **********************************************************************************/
 
-__device__ int match(char *check) {
-  char plainPassword_1[] = "BF9999";
-  char plainPassword_2[] = "CN9898";
-  char plainPassword_3[] = "BT9893";
-  char plainPassword_4[] = "MA5369";
+__device__ char* EncryptPassword(char* rawPassword){
 
-  char *a = check;
-  char *b = check;
-  char *c = check;
-  char *d = check;
-  char *p1 = plainPassword_1;
-  char *p2 = plainPassword_2;
-  char *p3 = plainPassword_3;
-  char *p4 = plainPassword_4;
+	char * newPassword = (char *) malloc(sizeof(char) * 11);
 
-  while(*a == *p1) { 
-   if(*a == '\0') 
-    {
-      printf("(Found) Password cracked is: %s\n",plainPassword_1);
-      break;
-    }
+	newPassword[0] = rawPassword[0] + 2;  
+	newPassword[1] = rawPassword[0] - 2;
+	newPassword[2] = rawPassword[0] + 1;  
+	newPassword[3] = rawPassword[1] + 3;
+	newPassword[4] = rawPassword[1] - 3;
+	newPassword[5] = rawPassword[1] - 1;
+	newPassword[6] = rawPassword[2] + 2;
+	newPassword[7] = rawPassword[2] - 2;
+	newPassword[8] = rawPassword[3] + 4;
+	newPassword[9] = rawPassword[3] - 4;
+	newPassword[10] = '\0';
 
-    a++;
-    p1++;
-  }
-	
-  while(*b == *p2) { 
-   if(*b == '\0') 
-    {
-      printf("(Found) Password cracked is: %s\n",plainPassword_2);
-      break;
-    }
+	for(int i =0; i<10; i++){
+        //checking all upper case letter limits
+        if(i >= 0 && i < 6){
+			if(newPassword[i] > 90){
+				newPassword[i] = (newPassword[i] - 90) + 65;
+			}else if(newPassword[i] < 65){
+				newPassword[i] = (65 - newPassword[i]) + 65;
+			}
+		}
+        //checking number section
+        else{
+            if(newPassword[i] > 57){
+				newPassword[i] = (newPassword[i] - 57) + 48;
+			}else if(newPassword[i] < 48){
+				newPassword[i] = (48 - newPassword[i]) + 48;
+			}
+		}
+	}
 
-    b++;
-    p2++;
-  }
-
-  while(*c == *p3) { 
-   if(*c == '\0') 
-    {
-      printf("(Found) Password cracked is: %s\n",plainPassword_3);
-      break;
-    }
-
-    c++;
-    p3++;
-  }
-
-  while(*d == *p4) { 
-   if(*d == '\0') 
-    {
-      printf("(Found) Password cracked is: %s\n",plainPassword_4);
-      return 1;
-    }
-
-    d++;
-    p4++;
-  }
-  return 0;
-
+    // Returns encyted password
+	return newPassword; 
 }
 
-
-__global__ void  kernel() {
-  char w,x,y,z;
-  char password[7];
-  password[6] = '\0';
-
-  int i = blockIdx.x+65;
-  int j = threadIdx.x+65;
-  char firstValue = i; 
-  char secondValue = j; 
-    
-password[0] = firstValue;
-password[1] = secondValue;
-
-for(w='0'; w<='9'; w++){
-  for(x='0'; x<='9'; x++){
-    for(y='0'; y<='9'; y++){
-      for(z='0'; z<='9'; z++){
-	  password[2] = w;
-	  password[3] = x;
-	  password[4] = y;
-	  password[5] = z; 
-	  if(match(password)) {
-	    printf("password found: %s\n", password);  
-	  } 
-          else {
-	     	  printf("(Processing) Brute Force Tried: %s\n", password); 
-	      }
-	   }
-	}
+__device__ int compareStrings(char* stringOne, char* stringTwo){
+	
+    while(*stringOne)
+    {
+        //Comparing the two strings
+        if (*stringOne != *stringTwo)
+            break;
+ 
+        //Changing Pointer location
+        stringOne++;
+        stringTwo++;
     }
-  }
-
+ 
+    // Returing the 0 if the two strings matches 
+    return *(const unsigned char*)stringOne - *(const unsigned char*)stringTwo;
 }
 
 int time_difference(struct timespec *start, struct timespec *finish, long long int *difference) {
-  long long int ds =  finish->tv_sec - start->tv_sec; 
-  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+    long long int ds =  finish->tv_sec - start->tv_sec; 
+    long long int dn =  finish->tv_nsec - start->tv_nsec; 
 
-  if(dn < 0 ) {
-    ds--;
-    dn += 1000000000; 
-  } 
-  *difference = ds * 1000000000 + dn;
-  return !(*difference > 0);
+    if(dn < 0 ) {
+        ds--;
+        dn += 1000000000; 
+    } 
+    *difference = ds * 1000000000 + dn;
+    return !(*difference > 0);
 }
 
+__global__ void kernel(char * alphabet, char * numbers, char * rawPassword){
 
-int main() {
+    char generatedRawPassword[4];
 
-  struct  timespec start, finish;
-  long long int time_elapsed;
-  clock_gettime(CLOCK_MONOTONIC, &start);
+    generatedRawPassword[0] = alphabet[blockIdx.x];
+    generatedRawPassword[1] = alphabet[blockIdx.y];
 
-  kernel <<<26,26>>>();
-  cudaDeviceSynchronize();
+    generatedRawPassword[2] = numbers[threadIdx.x];
+    generatedRawPassword[3] = numbers[threadIdx.y];
 
-  clock_gettime(CLOCK_MONOTONIC, &finish);
-  time_difference(&start, &finish, &time_elapsed);
-  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed, (time_elapsed/1.0e9)); 
-
-  return 0;
+    //Raw Password being encrypted
+    char *encPassword = EncryptPassword(rawPassword);
+        
+    //Comparing encrypted generated password with encrypted password 
+    if(compareStrings(EncryptPassword(generatedRawPassword), encPassword) == 0){
+        printf("Your password is cracked : %s = %s\n", generatedRawPassword, rawPassword);
+        printf("Your password Encrypted password : %s = %s\n", encPassword);
+        }
 }
+
+int main(int argc, char ** argv){
+
+    char cpuAlphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char cpuNumbers[10] = {'0','1','2','3','4','5','6','7','8','9'};
+
+    char * gpuAlphabet;
+    cudaMalloc( (void**) &gpuAlphabet, sizeof(char) * 26); 
+    cudaMemcpy(gpuAlphabet, cpuAlphabet, sizeof(char) * 26, cudaMemcpyHostToDevice);
+
+    char * gpuNumbers;
+    cudaMalloc( (void**) &gpuNumbers, sizeof(char) * 10); 
+    cudaMemcpy(gpuNumbers, cpuNumbers, sizeof(char) * 10, cudaMemcpyHostToDevice);
+
+    char * password;
+    cudaMalloc( (void**) &password, sizeof(char) * 26); 
+    cudaMemcpy(password, argv[1], sizeof(char) * 26, cudaMemcpyHostToDevice);
+
+    struct timespec start, finish;
+    long long int time_elapsed;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+        
+    kernel<<< dim3(26,26,1), dim3(10,10,1) >>>( gpuAlphabet, gpuNumbers, password);
+    cudaDeviceSynchronize();
+
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+    time_difference(&start, &finish, &time_elapsed);
+    printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed, (time_elapsed/1.0e9)); 
+
+    return 0;
+}
+
 
 
